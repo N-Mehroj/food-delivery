181a9508fe6185518bb741dd8e429dea980d8efd
N-Mehroj <nmehroj2004@mail.ru>
2024-04-16 18:09:03 +0500

    fix  and  changed

--- src/views/MyCardView.vue
+++ src/views/MyCardView.vue
@@ -10,11 +10,9 @@
           </div>
           <div class="md:flex w-full item-center justify-between py-4 border-b">
             <div class="flex gap-3 text-start">
-              <img
-                class="w-20 h-20 rounded-xl object-cover object-center"
+              <img class="w-20 h-20 rounded-xl object-cover object-center"
                 src="https://static.vecteezy.com/system/resources/thumbnails/030/201/658/small/beautiful-natural-landscape-of-green-meadows-rivers-and-mountains-free-photo.jpg"
-                alt=""
-              />
+                alt="" />
               <div class="">
                 <p class="text-sm font-semibold">Chicken & Ribs Combo</p>
                 <p class="p-0">
@@ -36,20 +34,15 @@
               </div>
               <div class="flex gap-5 lg:gap-7 items-center">
                 <span class="text-lg font-semibold">$12.40</span>
-                <v-icon
-                  name="bi-trash"
-                  class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500"
-                />
+                <v-icon name="bi-trash" class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500" />
               </div>
             </div>
           </div>
           <div class="md:flex w-full item-center justify-between py-4 border-b">
             <div class="flex gap-3 text-start">
-              <img
-                class="w-20 h-20 rounded-xl object-cover object-center"
+              <img class="w-20 h-20 rounded-xl object-cover object-center"
                 src="https://static.vecteezy.com/system/resources/thumbnails/030/201/658/small/beautiful-natural-landscape-of-green-meadows-rivers-and-mountains-free-photo.jpg"
-                alt=""
-              />
+                alt="" />
               <div class="">
                 <p class="text-sm font-semibold">Chicken & Ribs Combo</p>
                 <p class="p-0">
@@ -71,20 +64,15 @@
               </div>
               <div class="flex gap-5 lg:gap-7 items-center">
                 <span class="text-lg font-semibold">$12.40</span>
-                <v-icon
-                  name="bi-trash"
-                  class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500"
-                />
+                <v-icon name="bi-trash" class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500" />
               </div>
             </div>
           </div>
           <div class="md:flex w-full item-center justify-between py-4 border-b">
             <div class="flex gap-3 text-start">
-              <img
-                class="w-20 h-20 rounded-xl object-cover object-center"
+              <img class="w-20 h-20 rounded-xl object-cover object-center"
                 src="https://static.vecteezy.com/system/resources/thumbnails/030/201/658/small/beautiful-natural-landscape-of-green-meadows-rivers-and-mountains-free-photo.jpg"
-                alt=""
-              />
+                alt="" />
               <div class="">
                 <p class="text-sm font-semibold">Chicken & Ribs Combo</p>
                 <p class="p-0">
@@ -106,20 +94,15 @@
               </div>
               <div class="flex gap-5 lg:gap-7 items-center">
                 <span class="text-lg font-semibold">$12.40</span>
-                <v-icon
-                  name="bi-trash"
-                  class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500"
-                />
+                <v-icon name="bi-trash" class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500" />
               </div>
             </div>
           </div>
           <div class="md:flex w-full item-center justify-between py-4 border-b">
             <div class="flex gap-3 text-start">
-              <img
-                class="w-20 h-20 rounded-xl object-cover object-center"
+              <img class="w-20 h-20 rounded-xl object-cover object-center"
                 src="https://static.vecteezy.com/system/resources/thumbnails/030/201/658/small/beautiful-natural-landscape-of-green-meadows-rivers-and-mountains-free-photo.jpg"
-                alt=""
-              />
+                alt="" />
               <div class="">
                 <p class="text-sm font-semibold">Chicken & Ribs Combo</p>
                 <p class="p-0">
@@ -141,10 +124,7 @@
               </div>
               <div class="flex gap-5 lg:gap-7 items-center">
                 <span class="text-lg font-semibold">$12.40</span>
-                <v-icon
-                  name="bi-trash"
-                  class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500"
-                />
+                <v-icon name="bi-trash" class="cursor-pointer hover:text-rose-400 ease-in duration-100 text-gray-500" />
               </div>
             </div>
           </div>
@@ -157,14 +137,10 @@
       <div class="grid xl:grid-cols-1 md:grid-cols-2 gap-10">
         <div class="border rounded-2xl p-4">
           <div class="flex rounded-3xl bg-slate-200 p-1">
-            <button class="py-4 rounded-3xl w-1/2 text-lg font-medium">
-              Delivery
-            </button>
-            <button
-              class="py-4 bg-gray-800 rounded-3xl text-lg text-white w-1/2 font-medium"
-            >
-              Pick up
-            </button>
+            <input id="toggle-on" class="toggle toggle-left" name="toggle" value="false" type="radio" checked>
+            <label for="toggle-on" class="btn left  py-4 rounded-3xl w-1/2 text-black text-lg font-medium">Diario</label>
+            <input id="toggle-off" class="toggle toggle-right " name="toggle" value="true" type="radio">
+            <label for="toggle-off" class="btn rigth py-4  rounded-3xl text-lg text-black w-1/2 font-medium">Semanal</label>
           </div>
           <h4 class="text-base text-gray-400 font-normal mt-4">
             Restaurant address
@@ -188,9 +164,7 @@
             <p class="text-gray-600 font-semibold">Total</p>
             <span class="text-blue-600 font-semibold text-md">$129.40</span>
           </div>
-          <button
-            class="bg-[#4E60FF] h-12 text-white rounded-xl border transition-colors border-[#4E60FF] w-full mt-3"
-          >
+          <button class="bg-[#4E60FF] h-12 text-white rounded-xl border transition-colors border-[#4E60FF] w-full mt-3">
             To'lovni amalga oshirish
           </button>
         </div>
@@ -201,4 +175,62 @@
 <script>
 export default {};
 </script>
-<style></style>
+<style>
+.btn-frame {
+  border-radius: 30px;
+  border: 1px solid #1a1a1a;
+}
+
+.btn {
+  display: inline-block;
+  padding: 10px;
+  position: relative;
+  text-align: center;
+  transition: background 500ms ease-in, color 500ms ease-in;
+  z-index: 100;
+}
+
+input[type="radio"].toggle {
+	 display: none;
+}
+ input[type="radio"].toggle + label {
+	 cursor: pointer;
+	 min-width: 80px;
+}
+ input[type="radio"].toggle + label:hover {
+	 background: none;
+	 color: #1a1a1a;
+}
+ input[type="radio"].toggle + label:after {
+	 border-radius: 30px;
+	 background: #1a1a1a;
+	 content: "";
+	 height: 100%;
+	 position: absolute;
+	 top: 0;
+	 transition: left 250ms cubic-bezier(0.77, 0, 0.175, 1);
+	 width: 100%;
+	 z-index: -1;
+}
+ input[type="radio"].toggle.toggle-left + label {
+	 border-right: 0;
+}
+ input[type="radio"].toggle.toggle-left + label:after {
+	 left: 100%;
+}
+ input[type="radio"].toggle.toggle-right + label {
+	 margin-left: -5px;
+}
+ input[type="radio"].toggle.toggle-right + label:after {
+	 left: -100%;
+}
+ input[type="radio"].toggle:checked + label {
+	 cursor: default;
+	 color: #fff;
+	 transition: color 200ms;
+}
+ /* input[type="radio"].toggle:checked + label:after {
+	 left: 0;
+} */
+ 
+</style>
